1786a1eaa52981797eb254796c282c2575be5844
Azryel <AzryelShroud@gmail.com>
2021-05-23 18:29:24 +0200

    add new route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./App";
+import Posts from "./components/Posts";
 
 const Routes: any = () => {
     return (
       <Router>
         <Switch>
-          <Route path="/" component={App} />
+          <Route exact path="/" component={App} />
+          <Route exact path="/posts" component={Posts} />
         </Switch>
       </Router>
     );
