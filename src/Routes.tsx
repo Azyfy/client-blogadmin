c14e8a21618a47a26f2a9edc7417f30492617553
Azryel <AzryelShroud@gmail.com>
2021-05-24 17:02:06 +0200

    add new route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Posts from "./components/Posts";
 import SinglePost from "./components/SinglePost";
+import NewPost from "./components/NewPost";
 
 const Routes: any = () => {
     return (
@@ -10,6 +11,7 @@ const Routes: any = () => {
         <Switch>
           <Route exact path="/" component={App} />
           <Route exact path="/posts" component={Posts} />
+          <Route exact path="/posts/new" component={NewPost} />
           <Route path="/posts/:id" component={SinglePost} />
         </Switch>
       </Router>
