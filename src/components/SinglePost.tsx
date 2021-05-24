0d1173c31b794c80ce35ca2b2ad8003687a8883f
Azryel <AzryelShroud@gmail.com>
2021-05-24 19:34:02 +0200

    add delete option

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -6,12 +6,14 @@ const SinglePost = () => {
 
     const [ post, setPost ] = useState<any>();
     const [ loading, setLoading ] = useState(true);
+    const [ id, setId ] = useState("");
 
     let history = useHistory();
 
     useEffect( () => {
         
         let pathname = window.location.pathname.split("/");
+        setId(pathname[2]);
 
         axios.get(`http://localhost:3001/blogadmin/posts/${pathname[2]}`, {
             headers: {
@@ -29,15 +31,33 @@ const SinglePost = () => {
 
       }, []);
 
+      function deletePost () {
+        axios.delete(`http://localhost:3001/blogadmin/posts/${id}`,{
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("token")}`
+              }   
+        })
+        .then((res) => {
+            history.push("/posts");
+          })
+          .catch((error) => {
+            console.error(error)
+          })
+      }
+
 return(
     <div> 
         { (loading == true) ? (
             <p>Loading...</p>
         ) : (
+            <>
             <div>
                 <h4> {post.title} </h4>
                 <p> {post.text} </p>
             </div>
+
+            <button onClick={deletePost} type="button"> Delete </button>
+            </>
         )
         }
     </div>
